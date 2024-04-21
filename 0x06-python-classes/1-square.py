23a09e6ee8002920f1544de136f4e5b5214fea4d
jawd20 <jawd20@gmail.com>
2024-04-20 21:58:54 -0700

    getting the self attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,6 +5,8 @@ class Square:
     """this is a class that defines a squrare"""
 
     def __init__(self, size):
-        """initialize the data for the square."""
+        """initialize the data for the square.
+        size is the attribute of the square
+        """
 
         self.__size = size
