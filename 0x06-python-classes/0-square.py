942ac086709e5442291ab4cc76591a95a5f55c0a
jawd20 <jawd20@gmail.com>
2024-04-20 21:52:09 -0700

    understanding what it is for a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a class called Square"""
 
+
 class Square:
     """Empty class that defines a square"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
+
 class Square:
     """this is a class that defines a squrare"""
+
     def __init__(self, size):
-        """initialize the data"""
+        """initialize the data for the square."""
+
         self.__size = size
